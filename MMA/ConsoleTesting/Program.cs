ec98564856ef9802b7d76e4d4cdabb882e51ff04
Aytemir Ulanbek uulu <aytush2001@gmail.com>
2022-11-16 22:01:31 +0600

    service is worked

--- MMA/ConsoleTesting/Program.cs
+++ MMA/ConsoleTesting/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using MMA.Contract.Common;
 using MMA.DAL.Common;
 
 namespace ConsoleTesting
@@ -8,11 +9,16 @@ namespace ConsoleTesting
     {
         public static void Main(string[] args)
         {
-            using (var context = new CommonContext())
-            {
-                Console.WriteLine("start");
-                Console.WriteLine(context.Users.First().PhoneNumber);
-            }
+            using var service = new ServiceClientFactory<ICommonService>();
+            
+                var data = service.GetService().GetData(5);
+                Console.WriteLine(data);
+            
+            // using (var context = new CommonContext())
+            // {
+            //     Console.WriteLine("start");
+            //     Console.WriteLine(context.Users.First().PhoneNumber);
+            // }
         }
     }
 }
---
+++ MMA/Contract/MMA.Contract.Common/ServiceClientFactory.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Configuration;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Configuration;
+using System.ServiceModel.Description;
+
+namespace MMA.Contract.Common
+{
+    public class ServiceClientFactory<T> : IDisposable
+    {
+        private ChannelFactory<T> _channelFactory { get; }
+
+        public ServiceClientFactory()
+        {
+            _channelFactory = CreateChannelFactory();
+        }
+
+        public T GetService() => _channelFactory.CreateChannel();
+
+        private ChannelFactory<T> CreateChannelFactory()
+        {
+            var clientSection = ConfigurationManager.GetSection("system.serviceModel/client") as ClientSection;
+            if (clientSection == null)
+                throw new Exception("Нет секции для конфигурации клиента сервиса.");
+
+            var configurationName = typeof(T).Name;
+
+            var endpointElements = clientSection.Endpoints;
+                
+            
+            var endpointElement = endpointElements
+                .Cast<ChannelEndpointElement>()
+                .FirstOrDefault(c => c.Name == configurationName);
+
+            if (endpointElement == null)
+                throw new Exception($"Конечная точка для \"{configurationName}\" не задана.");
+
+            var endpoint = new EndpointAddress(endpointElement.Address);
+            var binding = new BasicHttpBinding();
+
+            return new ChannelFactory<T>(binding, endpoint);
+        }
+
+        public void Dispose()
+        {
+            if (_channelFactory.State == CommunicationState.Opened)
+                _channelFactory.Close();
+        }
+    }
+
+}
