fa7e4351eaeed8cbcf347a627c4327732cd1b050
Aytemir Ulanbek uulu <aytush2001@gmail.com>
2022-11-16 22:40:47 +0600

    added areas

---
+++ MMA/MMA.FrontMVC/Areas/Common/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MMA.FrontMVC.Areas.Common.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Common/Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
